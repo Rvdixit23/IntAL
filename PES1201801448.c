eb47611ba5114a9cb1bed976d538d340eb2ffb21
Rvdixit23 <rvdixit23@gmail.com>
2020-05-18 21:53:07 +0530

    Subtraction and exhaustive sampletest

--- PES1201801448.c
+++ PES1201801448.c
@@ -28,8 +28,8 @@ char* intal_add(const char *intal1, const char *intal2)
     const char* bigintal = len1 > len2 ? intal1 : intal2;
     const char* smallintal = len1 > len2 ? intal2 : intal1;
     // The answer
-    char *result = (char *)malloc(sizeof(char) * (bigLen + 1));
-    int resultIndex = bigLen; // Last index of answer
+    char *result = (char *)malloc(sizeof(char) * (bigLen + 2));
+    int resultIndex = bigLen; // Last index of answer exclude \0
     // Variables for step by step addition
     int carry = 0;
     int digitTotal;
@@ -64,6 +64,7 @@ char* intal_add(const char *intal1, const char *intal2)
     if (carry == 1 && resultIndex == 0)
     {
         result[0] = 49;
+        result[bigLen + 1] = '\0';
         return result;
     }
     else if (carry == 0)
@@ -75,11 +76,12 @@ char* intal_add(const char *intal1, const char *intal2)
         }
     }
     // To return a valid freeable pointer
-    char *result2 = (char *)malloc(sizeof(int) * bigLen);
+    char *result2 = (char *)malloc(sizeof(int) * (bigLen + 1));
     for (int i = 1; i <= bigLen; i++)
     {
         result2[i - 1] = result[i];
     }
+    result2[bigLen] = '\0';
     free(result);
     return result2;
     
@@ -87,6 +89,12 @@ char* intal_add(const char *intal1, const char *intal2)
 
 int intal_compare(const char *intal1, const char *intal2)
 {
+    /*
+    Compare lengths
+    If equal compare the digits until they aren't equal
+    If all digits are equal, then return 0
+    Else return accordingly
+    */
     int len1 = strlen(intal1);
     int len2 = strlen(intal2);
     if (len1 > len2)
@@ -106,7 +114,7 @@ int intal_compare(const char *intal1, const char *intal2)
             {
                 return 1;
             }
-            else
+            else if(intal1[lenCount] < intal2[lenCount])
             {
                 return -1;
             }
@@ -115,16 +123,124 @@ int intal_compare(const char *intal1, const char *intal2)
         if (lenCount == len1)
         {
             return 0;
-        }
-        
+        }   
     }
-    
     return 0;
-    
+
 }
+
+
 char* intal_diff(const char *intal1, const char *intal2)
 {
-    return 0;
+    /*
+    Find the larger and smaller number
+    Create an array of length of bigger number
+    This will manage the carry and borrow
+    Initialise to 0
+    Go through the smaller number
+        if larger than above digit
+        then add 10 to borrow arr
+        sub 1 from next place in borrow arr
+        Add the carry and the digits
+    Copy numbers from the big array
+    Run a loop to check for trialing zeros
+    Reallocate a new memory and copy the digits
+    Free the old result
+    Free the carry array
+    Return the result
+    */
+    int whichIsLarger = intal_compare(intal1, intal2);
+    const char *bigIntal;
+    const char *smallIntal;
+    if (whichIsLarger == 1)
+    {
+        bigIntal = intal1;
+        smallIntal = intal2;
+    }
+    else if (whichIsLarger == -1)
+    {
+        bigIntal = intal2;
+        smallIntal = intal1;
+    }
+    else
+    {
+        char *result = (char *)malloc(sizeof(char) * 2);
+        result[0] = '0';
+        result[1] = '\0';
+        return result;
+    }
+    int bigLen = strlen(bigIntal);
+    int smallLen = strlen(smallIntal);
+    char *result = (char *)malloc(sizeof(char) * (bigLen + 1));
+    result[bigLen] = '\0';
+    int *carry = (int *)malloc(sizeof(int) * bigLen);
+    for (int i = 0; i < bigLen; i++)
+    {
+        carry[i] = 0;
+    }
+    
+    int tempCarryIndex;
+    int currBig = bigLen - 1;
+    // If the bigNo + carry is less than small no
+    //  Go to next place, if greater than 0, carry of -1
+    //  If equal to 0 then go to next place, until you encounter a non zero
+    //  Add a -1 to the first non 0 location
+    //  Transfer the carry over until the current point
+    for (int currSml = smallLen - 1; currSml > -1; currSml--)
+    {
+        while (bigIntal[currBig] + carry[currBig]
+         < smallIntal[currSml])
+        {
+            carry[currBig] = carry[currBig] + 10;
+            carry[currBig - 1]--;
+        }
+        result[currBig] = carry[currBig] + bigIntal[currBig]
+         - smallIntal[currSml] + 48;
+        currBig--;
+    }
+    
+    while (currBig > 0)
+    {
+        // Copy the result from the bigger number
+        // Make sure the carry is handled
+        while (bigIntal[currBig] + carry[currBig] <= 48)
+        {
+            carry[currBig] = carry[currBig] + 10;
+            carry[currBig - 1]--;
+        }
+        result[currBig] = carry[currBig] + bigIntal[currBig];
+        currBig--;
+    }
+    result[0] = carry[0] + bigIntal[0];
+
+    
+    // Create a new string
+    // Without the trailing zeros of the older string
+    int nonZeroIndex = 0;
+    while (result[nonZeroIndex] == '0' && nonZeroIndex < bigLen)
+    {
+        nonZeroIndex++;
+    }
+    if (nonZeroIndex == 0)
+    {
+        // printf("Here\n");
+        result[bigLen] = '\0';
+        free(carry);
+        return result;
+    }
+    
+    char *finalRes = (char *)malloc(sizeof(char) * (bigLen - nonZeroIndex + 1));
+    finalRes[bigLen - nonZeroIndex] = '\0';
+    for (int i = 0; i < bigLen - nonZeroIndex; i++)
+    {
+        finalRes[i] = result[i + nonZeroIndex];
+    }
+    
+    // printf("Intermediate Res : %s\n", finalRes);
+    free(result);
+    free(carry);
+    return finalRes;
+
 }
 char* intal_multiply(const char *intal1, const char *intal2)
 {
@@ -144,7 +260,8 @@ char* intal_gcd(const char *intal1, const char *intal2)
 }
 char* intal_fibonacci(unsigned int n)
 {
-
+    return 0;
+    // REMOVE THIS LATER
     char *first = (char *)malloc(sizeof(char) * 2);
     first[1] = '\n';
     first[0] = '0';
