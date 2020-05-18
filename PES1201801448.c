348d0d5e301a9dbc00c9000a1c5f0c3ede4d28ac
Rvdixit23 <rvdixit23@gmail.com>
2020-05-18 11:50:09 +0530

    New version of assignment

--- PES1201801448.c
+++ PES1201801448.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<stdio.h>
 
-char *intal_add(char *intal1, char *intal2)
+char* intal_add(const char *intal1, const char *intal2)
 {
     /*
     Find the length of each intal
@@ -25,8 +25,8 @@ char *intal_add(char *intal1, char *intal2)
     int bigLen = len1 > len2 ? len1 : len2;
     int smallLen = len1 > len2 ? len2 : len1;
     // The intals with big and small determined
-    char* bigintal = len1 > len2 ? intal1 : intal2;
-    char* smallintal = len1 > len2 ? intal2 : intal1;
+    const char* bigintal = len1 > len2 ? intal1 : intal2;
+    const char* smallintal = len1 > len2 ? intal2 : intal1;
     // The answer
     char *result = (char *)malloc(sizeof(char) * (bigLen + 1));
     int resultIndex = bigLen; // Last index of answer
@@ -85,43 +85,98 @@ char *intal_add(char *intal1, char *intal2)
     
 }
 
-int intal_compare(char *intal1, char *intal2)
+int intal_compare(const char *intal1, const char *intal2)
+{
+    int len1 = strlen(intal1);
+    int len2 = strlen(intal2);
+    if (len1 > len2)
+    {
+        return 1;
+    }
+    else if (len2 > len1)
+    {
+        return -1;
+    }
+    else
+    {
+        int lenCount = 0;
+        while (lenCount < len1)
+        {
+            if (intal1[lenCount] > intal2[lenCount])
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+            lenCount++;
+        }
+        if (lenCount == len1)
+        {
+            return 0;
+        }
+        
+    }
+    
+    return 0;
+    
+}
+char* intal_diff(const char *intal1, const char *intal2)
 {
     return 0;
 }
-char* intal_diff(char* intal1, char* intal2)
+char* intal_multiply(const char *intal1, const char *intal2)
 {
     return 0;
 }
-char* intal_multiply(char* intal1, char* intal2)
+char* intal_mod(const char *intal1, const char *intal2)
 {
     return 0;
 }
-char* intal_mod(char* intal1, char* intal2)
+char* intal_pow(const char* intal1, unsigned int n)
 {
     return 0;
 }
-char* intal_pow(char* intal1, unsigned int n)
-{
-    return 0;
-}
-char* intal_gcd(char* intal1, char* intal2)
+char* intal_gcd(const char *intal1, const char *intal2)
 {
     return 0;
 }
 char* intal_fibonacci(unsigned int n)
 {
-    char *first = "0";
-    char *second = "1";
-    if (n == 0)
+
+    char *first = (char *)malloc(sizeof(char) * 2);
+    first[1] = '\n';
+    first[0] = '0';
+    char *second = (char *)malloc(sizeof(char) * 2);
+    second[1] = '\n';
+    second[0] = '1';
+
+    if (n == 1)
     {
         return first;
     }
-    if (n == 1)
+    if (n == 2)
     {
         return second;
     }
-    return '1';
+    /*
+    Convert first and second to const ints
+        This isn't needed as the addition function is read only
+        No need to make another const string just for this
+    Pass them into the adder and store this pointer
+    Make the old second into first
+    Make the sum as second and repeat
+    */
+    for (int i = 3; i < n + 1; i++)
+    {
+        char *sum = intal_add(first, second);
+        free(first);
+        first = second;
+        second = sum;
+    }
+    return second;
+    
     
 }
 char* intal_factorial(unsigned int n)
@@ -140,11 +195,11 @@ int intal_min(char **arr, int n)
 {
     return 0;
 }
-int intal_search(char **arr, int n, char* key)
+int intal_search(char **arr, int n, const char* key)
 {
     return 0;
 }
-int intal_binsearch(char **arr, int n, char* key)
+int intal_binsearch(char **arr, int n, const char* key)
 {
     return 0;
 }
@@ -157,3 +212,4 @@ char* coin_row_problem(char **arr, int n)
 {
     return 0;
 }
+
